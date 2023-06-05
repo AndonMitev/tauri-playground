d560305effdb44cd57520d7627a75a006cb1c5c1
AndonMitev <mitevandon94@gmail.com>
2023-06-05 10:24:31 +0300

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,27 @@
+'use client';
+import useDownloader from 'react-use-downloader';
+
+export default function Home() {
+  const { size, elapsed, percentage, download, cancel, error, isInProgress } =
+    useDownloader();
+
+  const fileUrl =
+    'https://upload.wikimedia.org/wikipedia/commons/4/4d/%D0%93%D0%BE%D0%B2%D0%B5%D1%80%D0%BB%D0%B0_%D1%96_%D0%9F%D0%B5%D1%82%D1%80%D0%BE%D1%81_%D0%B2_%D0%BF%D1%80%D0%BE%D0%BC%D1%96%D0%BD%D1%8F%D1%85_%D0%B2%D1%80%D0%B0%D0%BD%D1%96%D1%88%D0%BD%D1%8C%D0%BE%D0%B3%D0%BE_%D1%81%D0%BE%D0%BD%D1%86%D1%8F.jpg';
+  const filename = 'beautiful-carpathia.jpg';
+
+  return (
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+      <h1>Testing App</h1>
+      <p>Download is in {isInProgress ? 'in progress' : 'stopped'}</p>
+      <button onClick={() => download(fileUrl, filename)}>
+        Click to download the file
+      </button>
+      <button onClick={() => cancel()}>Cancel the download</button>
+      <p>Download size in bytes {size}</p>
+      <label htmlFor='file'>Downloading progress:</label>
+      <progress id='file' value={percentage} max='100' />
+      <p>Elapsed time in seconds {elapsed}</p>
+      {error && <p>possible error {JSON.stringify(error)}</p>}
+    </main>
+  );
+}
